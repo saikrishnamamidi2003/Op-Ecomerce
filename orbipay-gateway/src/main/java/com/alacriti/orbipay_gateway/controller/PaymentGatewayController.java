dbf7bcbdac95715e84a7e150ce3689a83d5d0213
Sai Krishna Mamidi <saikrishnam@saikrishnam-lp.ds.alacriti.com>
2026-08-01 16:18:34 +0530

    Add Correlation ID filter to gateway

--- orbipay-gateway/src/main/java/com/alacriti/orbipay_gateway/controller/PaymentGatewayController.java
+++ orbipay-gateway/src/main/java/com/alacriti/orbipay_gateway/controller/PaymentGatewayController.java
@@ -2,11 +2,15 @@ package com.alacriti.orbipay_gateway.controller;
 
 import com.alacriti.orbipay_gateway.dto.GatewayPaymentRequest;
 import com.alacriti.orbipay_gateway.dto.GatewayPaymentResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/gateway/payments")
 public class PaymentGatewayController {
@@ -28,7 +32,8 @@ public class PaymentGatewayController {
             throws InterruptedException {
 
        Thread.sleep(2000);
-
+        log.info("Gateway received request. Correlation ID: {}",
+                MDC.get("X-Correlation-ID"));
         return GatewayPaymentResponse.builder()
                 .paymentReference(request.getPaymentReference())
                 .status("SUCCESS")
